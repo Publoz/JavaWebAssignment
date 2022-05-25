493d3567e1378552f9b389900ac7799edb9796d1
Paul Ireland <publoz@Pauls-Air.hub>
2022-05-25 14:32:03 +1200

    Implement doGet for logs services

--- src/main/java/nz/ac/wgtn/swen301/resthome4logs/server/LogsServlet.java
+++ src/main/java/nz/ac/wgtn/swen301/resthome4logs/server/LogsServlet.java
@@ -12,15 +12,71 @@ public class LogsServlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+		
+		String limit = req.getParameter("limit");
+		String level = req.getParameter("level");
+		
+		if(limit == null || level == null) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+		
+		int num = -1;
+		try {
+			num = Integer.parseInt(limit);
+		} catch (NumberFormatException e) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+		
+		if(num < 0 || num > 2147483647) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+		
+		int levelNum = 0;
+		try {
+			levelNum = getLevel(level);
+		} catch (IllegalArgumentException e) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+		
+		
+		
+		
+		
 		PrintWriter out = resp.getWriter();
 		
-		resp.setContentType("text/html");
+		resp.setContentType("application/json");
 		
-		out.println("<html>");
-		out.println("<body>");
-		out.println("Hello world");
-		out.println("</body>");
-		out.println("</html>");
+		out.print(Persistency.getLogs(levelNum, num));
+
+	}
+	
+	private int getLevel(String level) {
+		switch (level) {
+		case "ALL":
+			return 0;
+		case "DEBUG":
+			return 1;
+		case "INFO":
+			return 2;
+		case "WARN":
+			return 3;
+		case "ERROR":
+			return 4;
+		case "FATAL":
+			return 5;
+		case "TRACE":
+			return 6;
+		case "OFF":
+			return 7;
+		default:
+			throw new IllegalArgumentException("Invalid level");
+		}
 	}
 
 	@Override
---
+++ src/main/java/nz/ac/wgtn/swen301/resthome4logs/server/Persistency.java
@@ -0,0 +1,58 @@
+package nz.ac.wgtn.swen301.resthome4logs.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Persistency {
+
+	public static List<JSONObject> logs = new ArrayList<JSONObject>();
+	
+	
+	
+	public static JSONArray getLogs(int level, int num){
+		//ArrayList<JSONObject> list = new ArrayList<>();
+		
+		JSONArray arr = new JSONArray(); 
+		int count = 0;
+		
+		
+		for(int i = logs.size() - 1; i >= 0; i--) {
+			if(getLevel(logs.get(i).getString("level")) >= level) {
+				arr.put(logs.get(i));
+				count++;
+				if(count >= num) {
+					break;
+				}
+			}
+		}
+		
+		return arr;
+	}
+	
+	public static int getLevel(String level) {
+		switch (level) {
+		case "ALL":
+			return 0;
+		case "DEBUG":
+			return 1;
+		case "INFO":
+			return 2;
+		case "WARN":
+			return 3;
+		case "ERROR":
+			return 4;
+		case "FATAL":
+			return 5;
+		case "TRACE":
+			return 6;
+		case "OFF":
+			return 7;
+		default:
+			throw new IllegalArgumentException("Invalid level");
+		}
+	}
+	
+}
